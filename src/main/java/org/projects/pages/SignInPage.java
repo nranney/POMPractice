d7921724accdafdac1bd95ed7cffd2fb0607b5bd
Natasha Ranney <natasha_ranney@persistent.com>
2024-01-16 14:47:06 +0530

    updated signin page

--- src/main/java/org/projects/pages/SignInPage.java
+++ src/main/java/org/projects/pages/SignInPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.projects.driver.BasePage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SignInPage extends BasePage {
 
@@ -13,6 +15,7 @@ public class SignInPage extends BasePage {
     public static final String NEXT_BTN = "//span[text()='Next']";
     public static final String PASSWORD_INPUT = "//input[@id='password']";
     public static final String SIGN_IN_BTN = "//button[@id='nextbtn']/span[text()='Sign in']";
+    public final static Logger LOGGER = LoggerFactory.getLogger(SignInPage.class);
 
     @FindBy(how= How.XPATH, using = SIGN_IN_HEADER )
     private WebElement signInHeader;
@@ -44,7 +47,10 @@ public class SignInPage extends BasePage {
         waitForElementToAppear(passwordInput);
         passwordInput.clear();
         passwordInput.sendKeys(password);
+        LOGGER.info("Sign in button being clicked : "+ signInBtn.getText());
         signInBtn.click();
+        LOGGER.info("Sign in button clicked");
+        pauseTest(2000);
         waitForURLToContainString("verify-email");
        // pauseTest(2000);
        }
